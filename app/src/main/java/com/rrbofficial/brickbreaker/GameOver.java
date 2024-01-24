5c424957f7f1c25199e55022f947ec7c08a93497
Rohit Balage <thatcomputerboy26@gmail.com>
2024-01-24 14:46:14 -0500

    Completed Brick breaker android game

---
+++ app/src/main/java/com/rrbofficial/brickbreaker/GameOver.java
@@ -0,0 +1,39 @@
+package com.rrbofficial.brickbreaker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameOver extends AppCompatActivity {
+
+    TextView tvPoints;
+    ImageView ivNewHighest;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_over);
+        ivNewHighest = findViewById(R.id.ivNewHeighest);
+        tvPoints = findViewById(R.id.tvPoints);
+        int points = getIntent().getExtras().getInt("points");
+        if (points == 240){
+            ivNewHighest.setVisibility(View.VISIBLE);
+        }
+        tvPoints.setText("" + points);
+    }
+
+    public void restart(View view){
+        Intent intent = new Intent(GameOver.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void exit(View view){
+        finish();
+    }
+}
